a7ac57dc80e36f656b93d946f771ff33a17d139a
andela-ddiei <delores.diei@andela.com>
2017-02-12 17:03:55 +0100

    set up gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: './src'
+    },
+    port: 3030,
+    ui: {
+      port: 3020
+    }
+  });
+
+  gulp.watch('src/*.js', browserSync.reload);
+  gulp.watch('src/*.css', browserSync.reload);
+  gulp.watch('src/*.html', browserSync.reload);
 });
+
