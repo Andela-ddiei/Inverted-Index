f694f8efa461f2553aa9ae1ff2cc7cfc266b415b
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-03-01 19:40:41 +0100

    get port from env variable

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
@@ -7,6 +8,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('app listening on 3000');
 })
